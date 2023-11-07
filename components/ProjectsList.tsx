7a7ae8053b36d077a694ccd96c033e113fbc176f
Yulya <iuliiaobr.io@gmail.com>
2023-11-07 20:29:11 +0700

    adds mock data and organize project one under another

--- components/ProjectsList.tsx
+++ components/ProjectsList.tsx
@@ -17,60 +17,45 @@
 import { Fragment, useState } from 'react'
 
 
-const breadcrumbs = [{ id: 1, name: 'Men', href: '#' }]
-const filters = [
-  {
-    id: 'color',
-    name: 'Color',
-    options: [
-      { value: 'white', label: 'White' },
-      { value: 'beige', label: 'Beige' },
-      { value: 'blue', label: 'Blue' },
-      { value: 'brown', label: 'Brown' },
-      { value: 'green', label: 'Green' },
-      { value: 'purple', label: 'Purple' },
+const projects = [
+    {
+    name: 'Spotify Clone',
+    href: '#',
+    price: '$220',
+    description:
+      "Don't compromise on snack-carrying capacity with this lightweight and spacious bag. The drawstring top keeps all your favorite chips, crisps, fries, biscuits, crackers, and cookies secure.",
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-featured-product-shot.jpg',
+    imageAlt: 'Model wearing light green backpack with black canvas straps and front zipper pouch.',
+    breadcrumbs: [
+      { id: 1, name: 'Travel', href: '#' },
+      { id: 2, name: 'Bags', href: '#' },
     ],
-  },
-]
-const products = [
-  {
-    id: 1,
-    name: 'Basic Tee 8-Pack',
+  },     {
+    name: 'Dashboard',
     href: '#',
-    price: '$256',
-    description: 'Get the full lineup of our Basic Tees. Have a fresh shirt all week, and an extra for laundry day.',
-    options: '8 colors',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg',
-    imageAlt: 'Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.',
-  },
-  {
-    id: 2,
-    name: 'Basic Tee',
-    href: '#',
-    price: '$32',
-    description: 'Look like a visionary CEO and wear the same black t-shirt every day.',
-    options: 'Black',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg',
-    imageAlt: 'Front of plain black t-shirt.',
-  },
-  // More products...
+    price: '$220',
+    description:
+      "Don't compromise on snack-carrying capacity with this lightweight and spacious bag. The drawstring top keeps all your favorite chips, crisps, fries, biscuits, crackers, and cookies secure.",
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-featured-product-shot.jpg',
+    imageAlt: 'Model wearing light green backpack with black canvas straps and front zipper pouch.',
+    breadcrumbs: [
+      { id: 1, name: 'Travel', href: '#' },
+      { id: 2, name: 'Bags', href: '#' },
+    ],
+  }
 ]
-
-
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(' ')
-}
+  
+//TODO: fix types
+  function classNames(...classes: any) {
+    return classes.filter(Boolean).join(' ')
+  }
 
 export default function ProjectsList() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
 
   return (
     <div className="text-white">
      
       <div>
-        {/* Mobile filter dialog */}
-
         <main className="mx-auto max-w-2xl px-4 lg:max-w-7xl lg:px-8">
           <div className="border-b border-gray-200 pb-10 pt-24">
             <h1 className="text-4xl font-bold tracking-tight text-white">Have a look at my projects</h1>
@@ -79,84 +64,46 @@ export default function ProjectsList() {
             </p>
           </div>
 
-          <div className="pb-24 pt-12 lg:grid lg:grid-cols-3 lg:gap-x-8 xl:grid-cols-4">
-            <aside>
-              <h2 className="sr-only">Filters</h2>
-
-              <button
-                type="button"
-                className="inline-flex items-center lg:hidden"
-                onClick={() => setMobileFiltersOpen(true)}
-              >
-                <span className="text-sm font-medium text-gray-700">Filters</span>
-                {/* <PlusIcon className="ml-1 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" /> */}
-              </button>
-
-              <div className="hidden lg:block">
-                <form className="space-y-10 divide-y divide-gray-200">
-                  {filters.map((section, sectionIdx) => (
-                    <div key={section.name} className='pt-10'>
-                      <fieldset>
-                        <legend className="block text-sm font-medium text-gray-900">{section.name}</legend>
-                        <div className="space-y-3 pt-6">
-                          {section.options.map((option, optionIdx) => (
-                            <div key={option.value} className="flex items-center">
-                              <input
-                                id={`${section.id}-${optionIdx}`}
-                                name={`${section.id}[]`}
-                                defaultValue={option.value}
-                                type="checkbox"
-                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                              />
-                              <label htmlFor={`${section.id}-${optionIdx}`} className="ml-3 text-sm text-gray-600">
-                                {option.label}
-                              </label>
-                            </div>
-                          ))}
-                        </div>
-                      </fieldset>
-                    </div>
-                  ))}
-                </form>
-              </div>
-            </aside>
-
-            <section aria-labelledby="product-heading" className="mt-6 lg:col-span-2 lg:mt-0 xl:col-span-3">
-              <h2 id="product-heading" className="sr-only">
-                Products
-              </h2>
-
-              <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:gap-x-8 xl:grid-cols-3">
-                {products.map((product) => (
-                  <div
-                    key={product.id}
-                    className="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
-                  >
-                    <div className="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
-                      <img
-                        src={product.imageSrc}
-                        alt={product.imageAlt}
-                        className="h-full w-full object-cover object-center sm:h-full sm:w-full"
-                      />
-                    </div>
-                    <div className="flex flex-1 flex-col space-y-2 p-4">
-                      <h3 className="text-sm font-medium text-gray-900">
-                        <a href={product.href}>
-                          <span aria-hidden="true" className="absolute inset-0" />
-                          {product.name}
-                        </a>
-                      </h3>
-                      <p className="text-sm text-gray-500">{product.description}</p>
-                      <div className="flex flex-1 flex-col justify-end">
-                        <p className="text-sm italic text-gray-500">{product.options}</p>
-                        <p className="text-base font-medium text-gray-900">{product.price}</p>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </section>
+            {projects.map((project) => (
+          <div key={project.name} className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+        {/* Product details */}
+        <div className="lg:max-w-lg lg:self-end">
+          <div className="mt-4">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{project.name}</h1>
           </div>
+
+          <section aria-labelledby="information-heading" className="mt-4">
+            <h2 id="information-heading" className="sr-only">
+              Product information
+            </h2>
+
+
+            <div className="mt-4 space-y-6">
+              <p className="text-base text-gray-500">{project.description}</p>
+            </div>
+
+            
+          </section>
+        </div>
+
+        {/* Product image */}
+        <div className="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center">
+          <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg">
+            <img src={project.imageSrc} alt={project.imageAlt} className="h-full w-full object-cover object-center" />
+          </div>
+        </div>
+
+        {/* Product form */}
+        <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
+          <section aria-labelledby="options-heading">
+            <h2 id="options-heading" className="sr-only">
+              Product options
+            </h2>
+
+          </section>
+        </div>
+      </div>
+            ))}
         </main>
 
       </div>
