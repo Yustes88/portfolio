869ae754a3fd2986367babaac154a9c89c491fb8
Yulya <iuliiaobr.io@gmail.com>
2023-11-08 20:08:19 +0700

    adds contact component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Contact from '@/components/Contact'
 import Header from '@/components/Header'
 import ProjectsList from '@/components/ProjectsList'
 
@@ -11,6 +12,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div>
           <ProjectsList/>
+          <Contact/>
         </div>
       </main>
 
