c1b9a0353e8161839ed939389ae90ee54a314089
Yulya <iuliiaobr.io@gmail.com>
2023-11-29 20:30:58 +0700

    adds resend to the project

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -46,6 +46,7 @@ export default function Contact() {
         <form method="POST" className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
         action={async(formData) => {
           await sendEmail(formData)
+          
         }}>
           <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
