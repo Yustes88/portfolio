1df2b4611019f358bf4ad2ed443c3589999aecc7
Yulya <iuliiaobr.io@gmail.com>
2023-11-30 18:55:52 +0700

    extracts server email and message validation to the utils file, adds try and catch block

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,26 +1,40 @@
 'use server'
 
+import { validateString } from '@/utils/utils'
 import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
+
 
 export const sendEmail = async(formData: FormData) => {
     const senderEmail = formData.get('email')
     const message = formData.get('message')
 
-    if(!message || typeof message !== 'string') {
-        return {
-            error: 'Invalid message'
-        }
+
+    if(!validateString(senderEmail, 500)) {
+      return {
+        error: 'Invalid sender email'
+      }  
     }
 
     
+    if(!validateString(message, 5000)) {
+        return {
+          error: 'Invalid message'
+        }  
+      }
 
-    resend.emails.send({
-        from: 'onboarding@resend.dev',
-        to: 'iuliiaobr.io@gmail.com',
-        subject: 'Message from portfolio contact form',
-        reply_to: senderEmail,
-        text: message,
-    })
+      try{
+          await resend.emails.send({
+              from: 'onboarding@resend.dev',
+              to: 'iuliiaobr.io@gmail.com',
+              subject: 'Message from portfolio contact form',
+              reply_to: senderEmail as string,
+              text: message as string,
+          })
+      } catch(error) {
+        return {
+            error: error.message
+        }
+      }
   }
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -75,6 +75,7 @@ export default function Contact() {
                     required
                     name="email"
                     id="email"
+                    maxLength={500}
                     placeholder='Your email'
                     autoComplete="email"
                     className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
@@ -92,7 +93,7 @@ export default function Contact() {
                     id="message"
                     placeholder='Your message'
                     required
-                    maxLength={500}
+                    maxLength={5000}
                     rows={4}
                     className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                     defaultValue={''}
