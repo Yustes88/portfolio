2e87bdf2bc157996bf4b5a2fe15043b6ac555169
Yustes88 <iuliiaobr.io@gmail.com>
2023-12-01 20:49:36 +0300

    adds skills to navigation, moves data to data file

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -3,7 +3,7 @@ import { skillsData } from "@/data/data";
 
 export default function Skills() {
     return(
-        <section className="mb-28 scroll-mt-28 text-center sm:mb-40">
+        <div className="mb-28 scroll-mt-28 text-center sm:mb-40" id="skills">
             <h2 className="text-3xl font-medium capitalize mb-8 text-center">
                 My skills
             </h2>
@@ -12,6 +12,6 @@ export default function Skills() {
                     <li key={index} className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80">{skill}</li>
                 ))}
             </ul>
-        </section>
+        </div>
     )
 }
