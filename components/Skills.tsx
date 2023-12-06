0b781634b215303293bcb6f0dd2f66ec7faed84c
Yulya <iuliiaobr.io@gmail.com>
2023-12-06 18:38:38 +0700

    adds animation to the skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,7 +2,21 @@
 
 import { skillsData } from "@/data/data";
 import { useSectionInView } from "@/hooks/useSectionInView";
+import { motion } from "framer-motion";
 
+const fadeInAnimationVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: (index: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.05 * index,
+      },
+    }),
+  };
 
 export default function Skills() {
     const { ref } = useSectionInView("Skills", 0.5);
@@ -12,9 +26,18 @@ export default function Skills() {
             <h2 className="text-3xl font-medium capitalize mb-8 text-center">
                 My skills
             </h2>
-            <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+            <ul className="grid grid-cols-5 gap-2 text-lg text-gray-800">
                 {skillsData.map((skill, index) => (
-                    <li key={index} className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80">{skill}</li>
+                    <motion.li
+                    className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+                    key={index}
+                    variants={fadeInAnimationVariants}
+                    initial="initial"
+                    whileInView="animate"
+                    custom={index}
+                  >
+                    {skill}
+                  </motion.li>
                 ))}
             </ul>
         </section>
