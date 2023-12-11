6d6e08d7e125b52fa55cc935bd78a81139a18e77
Yulya <iuliiaobr.io@gmail.com>
2023-12-11 17:58:23 +0700

    removes bulb

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -7,11 +7,7 @@ type SectionHeadingProps = {
 const Heading = ({children}: SectionHeadingProps) => {
   return (
     <h2 className=" sm:text-4xl text-2xl heading font-medium capitalize mb-8 text-center">
-    <span className='relative'>
     {children}
-    <TbBulb className='absolute -top-[25%] -right-[20%]'/>
-    </span>
-        
     </h2>
   )
 }
