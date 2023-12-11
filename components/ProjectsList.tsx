59b0e08d8549c15454239e0c50239e12b6185e85
Yulya <iuliiaobr.io@gmail.com>
2023-12-11 20:32:49 +0700

    adds unique bg color to each project

--- components/ProjectsList.tsx
+++ components/ProjectsList.tsx
@@ -1,11 +1,12 @@
 'use client'
-import Image from 'next/image'
 
 import ProjectItem from './ProjectItem'
 import { projects } from '@/data/data'
 import { useSectionInView } from '@/hooks/useSectionInView'
 import { motion } from 'framer-motion'
 import Heading from './Heading'
+
+import clsx from 'clsx'
 
 
 export default function ProjectsList() {
@@ -26,14 +27,16 @@ export default function ProjectsList() {
     }}
      className="text-white" id='projects' ref={ref}>
         <div className="mx-auto max-w-2xl px-4 lg:max-w-7xl lg:px-8">
-          <div className="border-b border-gray-200 pb-10 pt-24">
+          <div className="pb-10 pt-24">
             <Heading>My projects</Heading>
             <p className="mt-4 text-base text-gray-500">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, impedit? Dolorum, debitis harum. Sit illo dolorum excepturi obcaecati iusto ducimus recusandae in, facere doloribus vero ad suscipit, saepe ea a?
             </p>
           </div>
             {projects.map((project) => (
-                <ProjectItem key={project.name} data={project}/>
+              <div key={project.name} className={clsx('bg-gradient-to-tl to-[#1D1C21] to-50%', project.name === 'Spotify Clone' ? 'from-[#065f46]' : 'from-[#fff]')}>
+                <ProjectItem data={project}/>
+              </div>
             ))}
         </div>
     </motion.section>
--- data/data.ts
+++ data/data.ts
@@ -23,21 +23,19 @@ export const projects = [
     {
     name: 'Spotify Clone',
     href: 'https://spotify-clone-znti.vercel.app/',
-    price: '$220',
     description:
       "Don't compromise on snack-carrying capacity with this lightweight and spacious bag. The drawstring top keeps all your favorite chips, crisps, fries, biscuits, crackers, and cookies secure.",
     imageSrc: '/images/spotify.png',
     imageAlt: 'Model wearing light green backpack with black canvas straps and front zipper pouch.',
-    tools: ['NextJS, React, Tailwind, Typescript, Stripe, Supabase']
+    tools: ['NextJS, React, Tailwind, Typescript, Stripe, Supabase'],
   },     
   {
     name: 'Dashboard',
     href: 'https://blog-steel-alpha-98.vercel.app/',
-    price: '$220',
     description:
       "Don't compromise on snack-carrying capacity with this lightweight and spacious bag. The drawstring top keeps all your favorite chips, crisps, fries, biscuits, crackers, and cookies secure.",
     imageSrc: '/images/blog.png',
     imageAlt: 'Model wearing light green backpack with black canvas straps and front zipper pouch.',
-    tools: ['NextJS, React, Tailwind, Typescript, Stripe, Supabase']
+    tools: ['NextJS, React, Tailwind, Typescript, Stripe, Supabase'],
   }
 ]
