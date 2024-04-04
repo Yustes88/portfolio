e802176bfccec932410365e3ad9cd93f8a9ce9e1
Iuliia <iuliiaobr.io@gmail.com>
2024-04-04 20:15:35 +0700

    fix:fixed nav background and border color

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -20,7 +20,7 @@ export default function Nav({ navigation }: NavProps) {
     <>
       <div className="z-[999] relative">
         <motion.div
-          className="md:flex fixed hidden top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full bg-gray-950 border-black/40 bg-opacity-75"
+          className="md:flex fixed hidden top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-opacity-40 bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full bg-gray-950 border-black/40 bg-opacity-75"
           initial={{ y: -100, x: "-50%", opacity: 0 }}
           animate={{ y: 0, x: "-50%", opacity: 1 }}
         ></motion.div>
